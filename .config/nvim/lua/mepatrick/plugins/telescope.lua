e625deeb851267931eb931740de01dd05debcc0f
mepatrick73 <pameu17@ulaval.ca>
2024-03-26 17:35:50 -0400

    Adding lualine to nvim configuration

--- .config/nvim/lua/mepatrick/plugins/telescope.lua
+++ .config/nvim/lua/mepatrick/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
 
-    dependencies = { 
+    dependencies = {
         'nvim-lua/plenary.nvim',
         {
             'nvim-telescope/telescope-fzf-native.nvim',
