1ab51ebce604e05886207506924f95eaa38fd645
mepatrick73 <pameu17@ulaval.ca>
2024-04-19 18:19:07 -0400

    feat: adding clangd to formatting

--- .config/nvim/lua/mepatrick/plugins/formatting.lua
+++ .config/nvim/lua/mepatrick/plugins/formatting.lua
@@ -1,26 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    config = function()
+        local conform = require("conform")
 
-		require("conform").setup({
-			formatters_by_ft = {
-				python = { "autopep8" },
-				javascript = { { "prettierd", "prettier" } },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout = 500,
-			},
-		})
+        require("conform").setup({
+            formatters_by_ft = {
+                python = { "autopep8" },
+                javascript = { { "prettierd", "prettier" } },
+                cpp = { "clang-format" },
+                h = { "clang-format" }
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout = 500,
+            },
+        })
 
-		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-			conform.format({
-				lsp_fallbacl = true,
-				async = false,
-				timeout = 500,
-			})
-		end, { desc = "Format selected text or current file" })
-	end,
+        vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+            conform.format({
+                lsp_fallbacl = true,
+                async = false,
+                timeout = 500,
+            })
+        end, { desc = "Format selected text or current file" })
+    end,
 }
