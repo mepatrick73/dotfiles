4f4c7c7f78f9134a02acd4828858d1a33b3efe4c
mepatrick73 <pameu17@ulaval.ca>
2024-05-17 18:36:54 -0400

    snippet binding change
    changing bindings for snippets. Combining cmp select next with luasnip previous snippet thing. Most problem of this type of binding can be fixed using the closing keybing for cmp which happens to be C-e at the moment

--- .config/nvim/lua/mepatrick/plugins/lsp.lua
+++ .config/nvim/lua/mepatrick/plugins/lsp.lua
@@ -1,3 +1,6 @@
+function hello(how are you sir)
+    
+end
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -125,20 +128,26 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete({}),
                 ["<C-i>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-l>"] = cmp.mapping(function()
-                    if luasnip.expand_or_locally_jumpable() then
+                ["<C-n>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
+                    else
+                        fallback()
                     end
                 end, { "i", "s" }),
-                ["<C-8>"] = cmp.mapping(function()
-                    if luasnip.locally_jumpable(-1) then
+                ["<C-p>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
                         luasnip.jump(-1)
+                    else
+                        fallback()
                     end
                 end, { "i", "s" }),
             }),
