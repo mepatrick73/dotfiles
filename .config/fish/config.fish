3039d43fa1338b67906ecebbc1f7637e1292de71
Patrick <pameu17@ulaval.ca>
2026-04-02 01:06:48 -0400

    fish: add CUDA to PATH and LD_LIBRARY_PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,4 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 export PATH="$HOME/.local/bin:$PATH"
+set -x LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
